921268947815ba7bbfdcee83bf7a850127022252
Mentor Junior Java Developer <kontakt@juniorjavadeveloper.pl>
2023-04-09 20:09:19 +0200

    Added Model class for JDBC and Entity class for Hibernate along with Maven dependencies for Hibernate.

---
+++ src/main/java/pl/juniorjavadeveloper/java/juniortransitionmid/deepunderstanding/hibernate/orm_jdbc/UserEntity.java
@@ -0,0 +1,30 @@
+package pl.juniorjavadeveloper.java.juniortransitionmid.deepunderstanding.hibernate.orm_jdbc;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class UserEntity {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(unique = true)
+    private String login;
+    private String password;
+
+    @Column(name = "FIRST_NAME")
+    private String firstName;
+    @Column(name = "LAST_NAME")
+    private String lastName;
+
+    public UserEntity() {
+    }
+
+    public UserEntity(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+}
